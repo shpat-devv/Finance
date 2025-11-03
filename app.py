bc94bc97049c15d195fbb2c4b184fbbb6b108247
Shpat Avdiu <uglyprincess@Shpats-MacBook-Pro.fritz.box>
2025-11-03 14:24:56 +0100

    start command

--- app.py
+++ app.py
@@ -269,6 +269,5 @@ def register():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=1000, debug=True)
 
